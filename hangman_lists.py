0958fcdc416e1249d0e16a88ee09e64ce082abfc
InJoy09 <118420872+InJoy09@users.noreply.github.com>
2024-03-06 22:55:34 +0300

    Add files via upload

---
+++ hangman_lists.py
@@ -0,0 +1,46 @@
+
+words_eng = [
+	'person', 'country', 'system', 'attitude', 'problem', 'solution', 'history', 'history', 'thousand', 'opportunity',
+	'result', 'article', 'company', 'development', 'process', 'condition', 'beginning', 'minute', 'quality', 'action',
+	'society', 'activity', 'organization', 'president', 'moment', 'letter', 'situation', 'condition', 'apartment',
+	'apartment', 'attention', 'program', 'enterprise', 'conversation', 'government', 'production', 'information',
+	'position', 'interest', 'federation', 'management', 'movement', 'material', 'feeling', 'newspaper', 'comrade',
+	'culture', 'opinion', 'document', 'institute', 'project', 'meeting', 'director', 'service', 'composition',
+	'quantity', 'object', 'creation', 'meaning', 'period', 'structure', 'example', 'research', 'citizen', 'principle',
+	'principle', 'character', 'struggle', 'education', 'representative', 'participation', 'politics', 'picture',
+	'dollar', 'territory', 'change', 'direction', 'drawing', 'current', 'church', 'population', 'majority', 'freedom',
+	'memory', 'command', 'contract', 'master', 'nature', 'telephone', 'position', 'writer', 'spectacle', 'manager',
+	'specialist', 'estimate', 'region', 'percentage', 'parent', 'requirement', 'analysis', 'economy', 'literature',
+	'degree', 'master', 'subject', 'director', 'surface', 'feeling', 'station', 'revolution', 'ministry'
+]
+
+phrases_eng = [
+	'You need to guess the target word:', 'You letter:\n  ', 'Congratulation, You win!!!',
+	'Once again? (y, n)', 'GAME OVER\n', 'Goodbye\n'
+]
+
+words_rus = [
+	'человек', 'работа', 'вопрос', 'сторона', 'страна', 'случай', 'голова', 'ребенок', 'система', 'отношение',
+	'женщина', 'деньги', 'машина', 'проблема', 'решение', 'история', 'власть', 'тысяча', 'возможность', 'результат',
+	'область', 'статья', 'компания', 'группа', 'развитие', 'процесс', 'условие', 'средство', 'начало', 'уровень',
+	'минута', 'качество', 'дорога', 'действие', 'государство', 'любовь', 'взгляд', 'общество', 'деятельность',
+	'организация', 'президент', 'комната', 'порядок', 'момент', 'письмо', 'помощь', 'ситуация', 'состояние',
+	'квартира', 'внимание', 'смерть', 'программа', 'задача', 'предприятие', 'разговор', 'правительство',
+	'производство', 'информация', 'положение', 'интерес', 'федерация', 'правило', 'управление', 'мужчина', 'партия',
+	'сердце', 'движение', 'материал', 'неделя', 'чувство', 'газета', 'причина', 'основа', 'товарищ', 'культура',
+	'данные', 'мнение', 'документ', 'институт', 'проект', 'встреча', 'директор', 'служба', 'судьба', 'девушка',
+	'очередь', 'состав', 'количество', 'событие', 'объект', 'создание', 'значение', 'период', 'искусство',
+	'структура', 'пример', 'исследование', 'гражданин', 'начальник', 'принцип', 'воздух', 'характер', 'борьба',
+	'использование', 'размер', 'образование', 'мальчик', 'представитель', 'участие', 'девочка', 'политика', 'картина',
+	'доллар', 'территория', 'изменение', 'направление', 'рисунок', 'течение', 'церковь', 'население', 'большинство',
+	'музыка', 'правда', 'свобода', 'память', 'команда', 'договор', 'дерево', 'хозяин', 'природа', 'телефон',
+	'позиция', 'писатель', 'самолет', 'солнце', 'спектакль', 'способ', 'журнал', 'руководитель', 'специалист',
+	'оценка', 'регион', 'процент', 'родитель', 'требование', 'основание', 'половина', 'анализ', 'автомобиль',
+	'экономика', 'литература', 'бумага', 'степень', 'господин', 'надежда', 'предмет', 'руководство', 'площадь',
+	'режиссер', 'поверхность', 'ощущение', 'станция', 'революция', 'колено', 'министерство', 'стекло'
+]
+
+phrases_rus = [
+	'Вам нужно угадать загаданное слово:', 'Ваша буква:\n  ', 'Поздравляю, Вы победили!!!',
+	'Еще раз? (д, н)', 'GAME OVER\n', 'До свидания\n'
+]
